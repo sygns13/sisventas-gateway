9e22a4e854f7940b18835e5b5b3aff25cce2c3ed
Fernando Chavez <fernando.chavez@gdpteam.com>
2023-08-21 00:42:04 -0500

    ajustes Varios - avance venta parte cliente

--- src/main/java/com/bcs/gateway/sisventasgateway/ResourceServerConfig.java
+++ src/main/java/com/bcs/gateway/sisventasgateway/ResourceServerConfig.java
@@ -7,17 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableResourceServer
@@ -25,6 +27,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
     @Autowired
     private Environment env ;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -36,9 +41,17 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
     public void configure(HttpSecurity http) throws Exception {
         // TODO Auto-generated method stub
 
-        http.authorizeRequests().antMatchers("/api/security/oauth/**").permitAll()
+        http
+                .exceptionHandling().authenticationEntryPoint(new AuthException())
+                .and()
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/security/oauth/**").permitAll()
+                .antMatchers("/api/security/tokens/anular").permitAll()
                 .antMatchers("/api/backend/**").hasAnyAuthority("Super_Administrador")
-                .anyRequest().authenticated().and().cors()
+                //.anyRequest().authenticated()
+                .and().cors()
                 .configurationSource(configurationSource());
 
 
@@ -49,6 +62,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
                 .antMatchers("/api/withdrawal/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated().and().cors()
                 .configurationSource(configurationSource());*/
+/*
+        http
+                .exceptionHandling().authenticationEntryPoint(new AuthException())
+                .and()
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/tokens/anular/**" ).permitAll();*/
+
 
 
     }
@@ -79,9 +101,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 
     @Bean
-    public JwtTokenStore tokenStore() {
+    public JdbcTokenStore tokenStore() {
 
-        return new JwtTokenStore(accessTokenConverter());
+        //return new JwtTokenStore(accessTokenConverter());
+        return new JdbcTokenStore(this.dataSource);
     }
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
